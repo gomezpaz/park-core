1972fef9f273a42cfbf1542b59f6d92d1c7236b1
gomezpaz <santigomezpaz@gmail.com>
2021-11-19 20:43:27 -0700

    Initial commit for heroku

---
+++ app.py
@@ -0,0 +1,43 @@
+from os import write
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
+import streamlit.components.v1 as components
+import hydralit as hy
+# from computervision.motiontracker import VideoProcessor
+import pandas as pd
+import numpy as np
+
+# Set up RTC config for https protocol
+RTC_CONFIGURATION = RTCConfiguration(
+    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+)
+
+# Set title
+app = hy.HydraApp(title='Park Corps')
+
+
+@app.addapp(title='Home')
+def Home():
+    st.header("Park Corps")
+
+
+@app.addapp(title='Database')
+def Models():
+    st.header("Database")
+
+
+@app.addapp(title='Map')
+def Demo():
+    st.header("Map")
+
+    # webrtc_ctx = webrtc_streamer(
+    #     key="motion-tracker",
+    #     mode=WebRtcMode.SENDRECV,
+    #     rtc_configuration=RTC_CONFIGURATION,
+    #     video_processor_factory=VideoProcessor,
+    #     media_stream_constraints={"video": True, "audio": False},
+    #     async_processing=True,
+    # )
+
+
+app.run()
